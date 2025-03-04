0f6db4f0256bfd9094fe1b81c5b27fb0a3f5f1f1
Angela <angela.huang0117@gmail.com>
2025-03-05 00:51:08 +0800

    酒譜詳情第二區塊修改完成

--- src/pages/WineContent.jsx
+++ src/pages/WineContent.jsx
@@ -105,12 +105,11 @@ function WineContent() {
           <h2 className="fs-6 fs-md-5 fs-lg-3 text-primary-1" data-aos="zoom-in-up">品酒討論</h2>
         </div>
 
-        <div class="wine-comments-section bg-primary-1 px-6 px-md-15 py-10 py-md-11">
+        <div class="wine-comments-section bg-primary-1 px-6 px-md-15 py-10 py-md-11 mx-md-11">
           <div className="user-info" data-aos="fade-right">
             <img
               src="../assets/images/Ellipse 9.png"
-              alt="Angela's avatar"
-              className="avatar"
+              alt="Angela's avatar"             
             />
             <span className="eng-font fs-8 fs-md-7 text-primary-4 fw-bold">Angela</span>
             <span className="fs-9 fs-md-9 text-neutral-3">09-23-2024</span>
@@ -130,8 +129,7 @@ function WineContent() {
               <div className="wine-comments-list-info d-flex align-items-center">
                 <img
                   src="/assets/images/Ellipse 11.png"
-                  alt="Nate's avatar"
-                  className="avatar"
+                  alt="Nate's avatar"                 
                 />
                 <span className="eng-font fs-8 fs-md-7 text-primary-4 fw-bold pt-1"
                   >Nate</span>
@@ -147,8 +145,7 @@ function WineContent() {
               <div className="wine-comments-list-info d-flex align-items-center">
                 <img
                   src="/assets/images/Ellipse 8.png"
-                  alt="Chris's avatar"
-                  className="avatar"
+                  alt="Chris's avatar"                 
                 />
                 <span className="eng-font fs-8 fs-md-7 text-primary-4 fw-bold pt-1"
                   >Chris</span>
@@ -166,8 +163,7 @@ function WineContent() {
               <div className="wine-comments-list-info d-flex align-items-center">
                 <img
                   src="/assets/images/Ellipse 10.png"
-                  alt="Kasie's avatar"
-                  className="avatar"
+                  alt="Kasie's avatar"                  
                 />
                 <span className="eng-font fs-8 fs-md-7 text-primary-4 fw-bold pt-1"
                   >Kasie</span>
@@ -196,7 +192,7 @@ function WineContent() {
     {/* 第三區 */}
 
     <div className="container special-list">
-        <div className="text-center mb-6 mb-lg-13 pt-10 pt-lg-15" data-aos="zoom-in-up">
+        <div className="text-center mb-6 mb-lg-11 pt-10 pt-lg-13" data-aos="zoom-in-up">
           <p className="eng-font fs-8 fs-md-5 text-primary-1 mb-4">Specials</p>
           <h2 className="fs-6 fs-md-5 fs-lg-3 text-primary-1">還想來點特別的</h2>
         </div>
@@ -205,16 +201,16 @@ function WineContent() {
           <div
             className="special-list-btn d-flex justify-content-end align-items-center"
           >
-            <p className="fs-8 fs-md-5 me-2 me-md-6">查看更多</p>
+            <p className="fs-8 fs-md-7 me-2 me-md-6">查看更多</p>
             <span className="material-symbols-outlined fs-8 fs-md-5 me-6">
               arrow_forward_ios
             </span>
           </div>
         </a>
 
-    {/* 酒譜卡片 */}
+    {/* 酒譜卡片 此處需套用emma的卡片元件 尚未修改*/}
 
-    <div className="row gx-lg-13 gy-lg-13 gy-md-10 gx-md-6 flex-md-wrap flex-nowrap overflow-x-scroll scrollBar pb-10 pb-lg-15" data-aos="zoom-in">
+    <div className="row gx-lg-13 gy-lg-13 gy-md-10 gx-md-6 flex-md-wrap flex-nowrap overflow-x-scroll scrollBar pb-10 pb-lg-13" data-aos="zoom-in">
       <div className="col-lg-4 col-md-6 col-9 overflow-hidden">
         <div className="card-container">
           <div className="card special-list-card-bg position-relative">
