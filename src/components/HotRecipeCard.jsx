fc71531b0034ea7fea86d7a992fac1d6c9275e70
KasieLLL <kasie.nnnuio@gmail.com>
2025-03-12 12:57:52 +0800

    首頁熱門酒譜調整比例

--- src/components/HotRecipeCard.jsx
+++ src/components/HotRecipeCard.jsx
@@ -10,32 +10,33 @@ const HotRecipeCard = ({ recipe }) => {
                 <div className="wrap"></div>
             </div>
 
-            <div className="wine-card-body d-flex flex-column flex-lg-row justify-content-lg-between gap-3 gap-lg-11">
-                <div className="txt d-flex flex-column text-primary-4">
+            <div className="wine-card-body d-flex flex-column flex-lg-row justify-content-lg-between gap-lg-13 gap-4">
+                <div className="txt d-flex flex-column justify-content-between text-primary-4">
                     <div className="wine-name">
                         <h3 className="fs-6 fs-lg-4 mb-1 mb-lg-3 eng-font">
                             {recipe.title_en}
                         </h3>
                         <h4 className="fs-7 fs-lg-5">{recipe.title}</h4>
                     </div>
-                    <div className="wine-tags d-flex mt-6 mt-lg-13 mb-6 gap-4">
-                        {/* {recipe?.tags?.map((tag, index) => (
+                    <div className="mt-lg-auto mt-5">
+                        <div className="wine-tags d-flex gap-4">
+                            {/* {recipe?.tags?.map((tag, index) => (
                             <span key={index} className="d-block px-2 px-lg-4 py-1 py-lg-2 bg-primary-3 rounded-pill text-white">{tag}</span>
                         ))} */}
-                        {recipe.tags.map((tag, index) => (
-                            <span key={index} className="d-block px-2 px-lg-4 py-1 py-lg-2 bg-primary-3 rounded-pill text-white" >
-                                {tag}
-                            </span>
-                        ))}
-                    </div>
-                    <div className="wine-intro">
-                        {recipe.content}
+                            {recipe.tags.map((tag, index) => (
+                                <span key={index} className="d-block px-2 px-lg-4 py-1 py-lg-2 bg-primary-3 rounded-pill text-white" >
+                                    {tag}
+                                </span>
+                            ))}
+                        </div>
+                        <div className="wine-intro">
+                            {recipe.content}
+                        </div>
                     </div>
                 </div>
                 <div className="pic position-relative">
                     <img
-                        src={recipe?.imagesUrl?.[0] || "https://via.placeholder.com/300"}
-                        // src={recipe.imagesUrl[0]}
+                        src={recipe.imagesUrl[0]}
                         alt={recipe.title}
                     />
                     <div className="arrow position-absolute bottom-0 end-0">
