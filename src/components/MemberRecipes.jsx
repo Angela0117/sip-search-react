fa40be39511221f0174a056bd310024de773cf17
Angela <angela.huang0117@gmail.com>
2025-07-14 23:07:25 +0800

    新增 讓已收藏的酒譜卡片在會員收藏頁面中套用active效果

--- src/components/MemberRecipes.jsx
+++ src/components/MemberRecipes.jsx
@@ -1,33 +1,41 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+//import { useParams } from "react-router-dom";
 import { useUser } from "../contexts/UserContext";
 import RecipeCard from "../components/RecipeCard";
+import useFavoriteRecipes from "../hooks/useFavoriteRecipes"; // 引入 hook
 
 function MemberRecipes() {
 
-  const { dataAxios } = useUser();
-  const { id } = useParams();
-  const [favoriteRecipes, setFavoriteRecipes] = useState([]);
+  const { user, dataAxios } = useUser();
+  //const { id } = useParams();
+  const [userFavoriteRecipes, setUserFavoriteRecipes] = useState([]);//會員收藏酒譜資料
+  const { favoriteRecipes, toggleFavorite } = useFavoriteRecipes(); // ✅ 使用 hook
 
   useEffect(() => {
     const getFavoriteRecipes = async () => {
       try {
-        const userDetail = await dataAxios.get(`/users/${id}`);
+        //const userDetail = await dataAxios.get(`/users/${id}`);
         const allRecipesRes = await dataAxios.get(`/recipes`);
-        const favoriteIds = userDetail.data.favorite_recipes;
+        //const favoriteIds = userDetail.data.favorite_recipes;
+
+        // const favorites = allRecipesRes.data.filter(recipe =>
+        //   favoriteIds.includes(recipe.id)
+        // );
 
         const favorites = allRecipesRes.data.filter(recipe =>
-          favoriteIds.includes(recipe.id)
+          user?.favorite_recipes?.includes(recipe.id)
         );
 
-        setFavoriteRecipes(favorites);
+        setUserFavoriteRecipes(favorites);
       } catch (error) {
         console.error("取得收藏酒譜失敗", error);
       }
     };
 
-    getFavoriteRecipes();
-  }, []);
+    if (user?.favorite_recipes) {
+      getFavoriteRecipes();
+    }
+  }, [user.favorite_recipes]);
 
 
 
@@ -39,8 +47,13 @@ function MemberRecipes() {
           className="row gx-lg-6 gy-lg-6 gy-md-3 gx-md-6 flex-md-wrap flex-nowrap overflow-x-scroll scrollBar pb-10 pb-lg-13"
           data-aos="zoom-in"
         >
-          {favoriteRecipes.map((recipe) => (
-            <RecipeCard key={recipe.id} recipe={recipe} />
+          {userFavoriteRecipes.map((recipe) => (
+            <RecipeCard
+              key={recipe.id}
+              recipe={recipe}
+              onFavorite={() => toggleFavorite(recipe.id)} //  用 hook 方法
+              isFavorite={favoriteRecipes.includes(recipe.id)} //  用 hook 狀態
+            />
           ))}
         </div>
 
